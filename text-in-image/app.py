f082e3183254ab0d4c355872b290e2b18fcbcc49
iceyao <yao3690093@gmail.com>
2024-12-05 16:56:29 +0800

    add .gitignore

--- text-in-image/app.py
+++ text-in-image/app.py
@@ -66,7 +66,8 @@ def generate_caption():
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": f"data:image/jpeg;base64,{base64_image}"
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                                "detail": "high"
                             }
                         }
                     ]
